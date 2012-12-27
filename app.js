50049c8412fd13baef2beabc6a0a137be09c2bf3
Sean Nessworthy <bladescope@googlemail.com>
2012-12-27 19:40:23 +0000

    Added some more core functions.

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ function App(options) {
 	 * @return {Boolean} TRUE if the bind was a success. FALSE if not.
 	 */
 	function bind (fn_name, fn) {
+		
+		logName = [settings.debug.nameSpaceCore,'bind'];
+		
 		if(typeof fn_name == 'string') {
 			if(typeof fn == 'function') {
 				
@@ -48,20 +51,82 @@ function App(options) {
 				
 				if(!functionExists || (functionExists && allowedToOverwrite === true)) {
 					functions[fn_name] = fn;
+					return true;
 				} else {
-					log([settings.debug.nameSpaceCore,'bind'], 'A function already exists called '+fn_name+', and the app does not have permission to overwrite.', 'error');
+					log(logName, 'A function already exists called '+fn_name+', and the app does not have permission to overwrite.', 'error');
 				}
 				
 			} else {
-				log([settings.debug.nameSpaceCore,'bind'], ['fn parameter passed needs to be a function.',fn], 'error');
+				log(logName, ['fn parameter passed needs to be a function.',fn], 'error');
 			}
 		} else {
-			log([settings.debug.nameSpaceCore,'bind'], ['Function name needs to be a string.',fn_name], 'error');
+			log(logName, ['Function name needs to be a string.',fn_name], 'error');
 		}
+		
+		return false;
+		
 	}
 	
+	/**
+	 * Call - calls a globally bound function. Accepts a function name (string) and an options parameter.
+	 * @param {String} fn_name The function name.
+	 * @param {Mixed} options Any options you wish to pass directly into the function.
+	 * @return {Mixed} FALSE if the function failed, or the function's return value.
+	 */
 	function call (fn_name, options) {
+		logName = [settings.debug.nameSpaceCore,'call'];
 		
+		if(typeof fn_name == 'string') {
+				
+			functionExists = false;
+			
+			if(typeof functions[fn_name] == 'function') {
+				functionExists = true;
+			}
+			
+			if(functionExists) {
+
+				return functions[fn_name](options);
+				
+			} else {
+				log(logName, 'A global function called '+fn_name+', doesn\'t exist, or hasn\'t been bound yet.', 'warn');
+			}
+			
+		} else {
+			log(logName, ['Function name needs to be a string.',fn_name], 'error');
+		}
+		
+		return false;
+	}
+
+	/**
+	 * Watch - Add a callback trigger for when an event is fired.
+	 * @param {String} trigger_name A string containing the event's name.
+	 * @param {Function} callback A function which is called when the event is fired.
+	 * @return {Boolean} TRUE if the callback is successfully added. FALSE if not.
+	 */
+	function watch(trigger_name, callback) {
+		logName = [settings.debug.nameSpaceCore,'watch'];
+		
+		if(typeof trigger_name == 'string') {
+			if(typeof callback == 'function') {
+				
+				if(typeof events[trigger_name] == 'undefined') {
+					events[trigger_name] = [];
+				}
+				
+				events[trigger_name].push(callback);
+				
+				return true;
+				
+			} else {
+				log(logName, ['callback parameter required, expected a function.',callback], 'error');
+			}
+		} else {
+			log(logName, ['trigger_name parameter required, expected a string.',trigger_name], 'error');
+		}
+		
+		return false;
 	}
 
 	/**
